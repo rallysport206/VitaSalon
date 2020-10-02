df0df7751c5f82a7bb6e77cba64a7677ab3d349c
rallysport206 <phan.tony125@gmail.com>
2020-10-01 22:26:01 -0700

    renamed header to nav. Now nav componet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
+import Nav from './Nav';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <Header />
+          <Nav />
           test 
         </p>
         <a
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav(){
+    return (
+        <div className="Navbar">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#contact">Contact</a>
+        </div>
+    )
+}
+
+export default Nav;
