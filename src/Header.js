9d9603c4768109c34390d45f062407401a620cf7
rallysport206 <phan.tony125@gmail.com>
2020-10-01 22:17:42 -0700

    header componet built

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+function Header(){
+    return (
+        <div className="Header">
+            <header>
+                <h1>TEST HEADER</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
