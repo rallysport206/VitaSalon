226e18c6d97c313ad053c20c1576dd71f8aa69ea
rallysport206 <phan.tony125@gmail.com>
2020-12-27 21:43:59 -0800

    updated with new stub

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <Footer />
+        </div>
+    )
+};
+
+export default Home;
