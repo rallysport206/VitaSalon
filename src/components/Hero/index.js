0b4176a20e5f17c5a7bb2669be9d1e66a0d423c4
rallysport206 <phan.tony125@gmail.com>
2021-01-13 22:37:03 -0800

    break push

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -4,23 +4,23 @@ import store from '../images/vitaFront.png';
 
 const Hero = () => {
     return(
-        <div className='hero-container'>
-            <div className='column-left'>
+        <div className='hero-section'>
+            <div className='hero-section__background'>
+                <img className="hero-section__image" src={store} alt='store front' /> 
+            </div>
+            <div className='content'>
                 <h1>Welcome to Vita Beauty Salon</h1>
                 <p>Serving you in the heart of Greenwood </p>
                 <p className='hero-info'><a href='https://goo.gl/maps/5KFeMShkoquPxpMb8' target='_blank' rel='noopener noreferrer'>
                 326 NW 85th St
                 <br />
                 Seattle, WA 98117
-                <br />
+                <br />  
                 </a>
                 <a href='tel:2067832031' target='_blank' rel='noopener noreferrer'>(206) 783 - 2031</a>
                 </p>
                 <p>Call to schedule an appointment!</p>
                 {/* <button>Contact Us for an Appointment!</button> */}
-            </div>
-            <div className='column-right'>
-                {/* <img src={store} alt='barber' className='hero-image'></img> */}
             </div>
         </div>
     )
